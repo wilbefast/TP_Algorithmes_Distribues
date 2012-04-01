7827b6f46f1138e6f1b93df2b5f38e9fb46402f6
William <wilbefast@gmail.com>
2012-04-01 14:28:38 +0200

    working duplex (server/client)

--- sdl_sockets/main.cpp
+++ sdl_sockets/main.cpp
@@ -14,11 +14,11 @@ int main(int argc, char **argv)
 
 	/* Open a socket on random port */
   UDPsocket socket;
-	ASSERT_NET(socket = SDLNet_UDP_Open(0), "Opening UDP socket");
+	ASSERT_NET(socket = SDLNet_UDP_Open(49152), "Opening UDP socket");
 
 	/* Resolve server name  */
 	IPaddress server;
-	ASSERT_NET(SDLNet_ResolveHost(&server, LOCALHOST, 1234) != -1,
+	ASSERT_NET(SDLNet_ResolveHost(&server, LOCALHOST, 49152) != -1,
               "Resolving host");
 
 	/* Allocate memory for the packet */
@@ -29,11 +29,13 @@ int main(int argc, char **argv)
 	bool stop = false;
 	while (!stop)
 	{
+	  /* SEND PACKET */
 		printf("Fill the buffer\n>");
 		scanf("%s", (char *)packet->data);
 
 		packet->address.host = server.host;	/* Set the destination host */
 		packet->address.port = server.port;	/* And destination port */
+		printf("\tAddress: %x %x\n", packet->address.host, packet->address.port);
 
 		packet->len = strlen((char *)packet->data) + 1;
 		SDLNet_UDP_Send(socket, -1, packet); /* This sets the packet->channel */
@@ -41,8 +43,25 @@ int main(int argc, char **argv)
 		/* Quit if packet contains "quit" */
 		if (!strcmp((char *)packet->data, "quit"))
 			stop = true;
+
+    /* RECEIVE PACKET */
+		if (SDLNet_UDP_Recv(socket, packet))
+		{
+			printf("UDP Packet incoming\n");
+			printf("\tChan:    %d\n", packet->channel);
+			printf("\tData:    %s\n", (char *)packet->data);
+			printf("\tLen:     %d\n", packet->len);
+			printf("\tMaxlen:  %d\n", packet->maxlen);
+			printf("\tStatus:  %d\n", packet->status);
+			printf("\tAddress: %x %x\n", packet->address.host, packet->address.port);
+
+			/* Quit if packet contains "quit" */
+			if (strcmp((char *)packet->data, "quit") == 0)
+				stop = true;
+		}
 	}
 
+  /* Clean up */
 	SDLNet_FreePacket(packet);
 	SDLNet_Quit();
 
